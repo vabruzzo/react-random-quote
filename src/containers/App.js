4aa4650b46a4d4af11a496146e429daf6fbe629b
vgabruzzo <vgabruzzo@gmail.com>
2015-11-23 03:52:53 -0500

    add container component

---
+++ src/containers/App.js
@@ -0,0 +1,35 @@
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import Quote from '../components/Quote';
+import * as QuoteActions from '../actions/quotes'
+
+export default class App extends Component {
+  render() {
+    const { quotes, actions } = this.props
+    return (
+      <div>
+        <Quote quotes={quotes} />
+        <button onClick={actions.prevQuote}>previous</button>
+        <button onClick={actions.nextQuote}>next</button>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    quotes: state.quotes
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(QuoteActions, dispatch)
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)

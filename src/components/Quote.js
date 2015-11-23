d34ed8e16250b5d1e3b0bc600eb1edeec41c41ff
Vincent Abruzzo <vabruzzo@hampshire.edu>
2015-11-23 12:52:07 -0500

    remove PropTypes

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
 
 export default class Quote extends Component {
   render() {
@@ -6,7 +6,6 @@ export default class Quote extends Component {
       <div>
         <h2>{this.props.quotes.quotes[this.props.quotes.currentQuote].text}</h2>
         <h3>- {this.props.quotes.quotes[this.props.quotes.currentQuote].author}</h3>
-
       </div>
     )
   }
